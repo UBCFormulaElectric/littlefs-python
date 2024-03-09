ba9975251a8be63dc1998472a1a5369c792bc68f
Jiajun-Huang <jhuang430@qq.com>
2024-03-09 13:58:40 -0800

    add windisk example

--- examples/windisk.py
+++ examples/windisk.py
@@ -1,21 +1,33 @@
+import time
+
 from littlefs import LittleFS, UserContextWinDisk
+
+# number = -1
+# with open("2", "rb") as f:
+#     data = f.read(32)
+#     # take first 4 bytes
+#     new_number = int.from_bytes(data[:4], byteorder='little')
+#     print(new_number)
+#     if(number + 1 != new_number):
+#         print("worng number")
+#     number = new_number
 
 disk_path = r"\\.\D: " # assume F drive is your storage device for littlefs
 
+bc_16g = 30228480
+bc_2g = bc_16g // 8 
+fs = LittleFS(block_size=512, block_count=1000000, mount=False ,context=UserContextWinDisk(disk_path))
 
-fs = LittleFS(block_size=512, block_count=30228480, mount=False, context=UserContextWinDisk(disk_path))
 
+# fs.format()
 fs.mount()
-fs.format()
 
 files = fs.listdir('/')
 for file in files:    
     with fs.open(file, 'rb') as fh:
-        data = fh.read(1000)
-        print(file)
-        print(data)
-    
-with fs.open("canMsg", 'rb') as fh:
-    # dump all can messages to a binary file to another storage devic
-    with open("canMsg", 'wb+') as f:
-        f.write(fh.read())
+        with open(file, 'wb+') as f:
+            data = fh.read()
+            print(data)
+            f.write(data)
+
+
--- how_to_install.md
+++ how_to_install.md
@@ -2,3 +2,9 @@ git submodule update --init --recursive
 pip install Cython
 pip install build
 python3 setup.py install --user
+
+
+```
+pip install -r requirements.txt
+pip install -e .
+```
